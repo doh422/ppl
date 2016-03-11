83822def998d4fb83c874b350ef9e817a1049856
Daniel Oh <doh422@gmail.com>
2016-03-10 16:15:09 -0800

    block object constructor and objects created

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// block objects
-var green = {
-	img: "assets/green.png"
-};
-var blue = {
-	img: "assets/blue.png"
-};
-var red = {
-	img: "assets/red.png"
-};
-var yellow = {
-	img: "assets/yellow.png"
-};
-var purple = {
-	img: "assets/purple.png"
+// block object constructor
+var Block = function(color, imgsrc) {
+	this.color = color;
+	this.imgsrc = imgsrc;
 };
 
+// block objects
+var green = new Block('green', 'assets/green.png');
+var blue = new Block('blue', 'assets/blue.png');
+var red = new Block('red', 'assets/red.png');
+var yellow = new Block('yellow', 'assets/yellow.png');
+var purple = new Block('purple', 'assets/purple.png');
+
+// jquery code
 $(document).ready(function() {
 	$('img').click(function() {
 		$(this).effect('explode');
